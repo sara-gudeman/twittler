8d8e2f811dd3252d21cdbb450a91f2c7d38cd0be
SARA GUDEMAN <sgudeman982@gmail.com>
2015-06-04 12:12:55 -0700

    Experimenting with .each jQuery function. Parameters are reversed in comparison to vanilla JS.

--- application.js
+++ application.js
@@ -1,14 +1,20 @@
+// NOTE: jQuery .each fcn takes in index first THEN value! Reverse of in vanilla JS
+$.each(streams.home, function(index, value) {
+  console.log(value.message);
+});
+
 $(document).ready(function(){
   var $body = $('body');
   $body.html('');
 
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    // console.log(tweet);
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
-    index -= 1;
-  }
+  // var index = streams.home.length - 1;
+  // while(index >= 0){
+  //   var tweet = streams.home[index];
+  //   // console.log(tweet);
+  //   var $tweet = $('<div></div>');
+  //   $tweet.text('@' + tweet.user + ': ' + tweet.message);
+  //   $tweet.appendTo($body);
+  //   index -= 1;
+  // }
+// };
 });
