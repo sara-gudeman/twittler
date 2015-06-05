43eb846f5732bedfc452c47ff316e5b9fde715b3
SARA GUDEMAN <sgudeman982@gmail.com>
2015-06-04 20:46:59 -0700

    Rewrote while loop using .each(). Cleaned up HTML set-up w/variables to make it more readable.

--- application.js
+++ application.js
@@ -1,22 +1,25 @@
 // NOTE: jQuery .each fcn takes in index first THEN value! Reverse of in vanilla JS
-$.each(streams.home, function(index, value) {
-  console.log(value.message);
-});
+// $.each(streams.home, function(index, value) {
+//   $('.container').append('<div>' + value + '</div>');
+// });
 
 $(document).ready(function(){
   var $body = $('body');
   // setting up HTML structure
-  $body.append('<div class="container"></div>');
-  $('.container').append('<button>Refresh Tweets</button>');
-
+  var $container = $('<div class="container"></div>');
+  $body.append($container);
+  $container.append('<button>Refresh Tweets</button>');
+  $.each(streams.home, function(index, tweetObj) {
+    var $tweet = $('<div class="tweet"></div>');
+    $tweet.text('@' + tweetObj.user + ': ' + tweetObj.message);
+    $tweet.appendTo($container);
+  });
   // var index = streams.home.length - 1;
   // while(index >= 0){
   //   var tweet = streams.home[index];
-  //   // console.log(tweet);
   //   var $tweet = $('<div></div>');
   //   $tweet.text('@' + tweet.user + ': ' + tweet.message);
   //   $tweet.appendTo($body);
   //   index -= 1;
   // }
-// };
 });
